49de4829fb768f5adcd40c708d75fe6056c054ab
Dong <dongnguyen030293@gmail.com>
2025-03-31 21:24:42 +0700

    thao-tac-voi-setstate

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,16 +4,38 @@ import React from "react"
 class Mycomponent extends React.Component {
 
     state = {
-        name: "dongnguyen",
+        name: "dong",
         address: "bienhoa",
         age: 32
+    }
+
+    handleClick(event) {
+        console.log("My event click!")
+        console.log("My name is ", this.state.name)
+
+        this.setState({
+            name: "dongnguyen",
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+
+    }
+
+    handleonMouseOver(event) {
+        console.log(event.pageX);
     }
 
     render() {
         return (
             <div>
                 My name is {this.state.name} I'm {this.state.age}
-            </div>
+                <div>
+                    <button onClick={(event) => { this.handleClick(event) }}  >click me</button>
+                    <button onMouseOver={(event) => { this.handleonMouseOver(event) }}
+                        onClick={(event) => { this.handleClick(event) }}  >hover me</button>
+                </div>
+
+
+            </div >
         )
     }
 }
