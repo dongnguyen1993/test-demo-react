cfdc5df64062e40ece7b76ce36af913d28141df1
TAEKWANG\nl.dong <dongnguyen030293@gmail.com>
2025-04-09 15:32:16 +0700

    lam-viec-voi-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.scss';
 import Header from './components/Header/Header';
-
+import { Link } from 'react-router-dom';
 
 
 
@@ -10,7 +10,17 @@ const App = () => {
   return (
     <div className="App-container">
       <Header />
-
+      <div>
+        Link router
+      </div>
+      <div>
+        <button>
+          <Link to={"/Admins"}>Admin</Link>
+        </button>
+        <button>
+          <Link to={"/Users"}>User</Link>
+        </button>
+      </div>
     </div>
   );
 }
